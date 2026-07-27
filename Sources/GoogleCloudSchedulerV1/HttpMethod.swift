cf5831072825d7835f3a4d0405619f7be9eca7b9
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleCloudSchedulerV1/HttpMethod.swift
+++ Sources/GoogleCloudSchedulerV1/HttpMethod.swift
@@ -89,7 +89,7 @@ public enum HttpMethod: Codable, Equatable, Sendable {
 
   /// Initialize from a string value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownStringValue`](doc:HttpMethod/unknownStringValue(_:)).
   public init(stringValue: Swift.String) {
     switch stringValue {
     case "HTTP_METHOD_UNSPECIFIED": self = .unspecified
@@ -106,7 +106,7 @@ public enum HttpMethod: Codable, Equatable, Sendable {
 
   /// Initialize from an integer value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownIntValue`](doc:HttpMethod/unknownIntValue(_:)).
   public init(intValue: Int) {
     switch intValue {
     case 0: self = .unspecified
